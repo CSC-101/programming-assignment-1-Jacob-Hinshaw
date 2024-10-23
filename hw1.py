306d76f8044f6987858fa1c7b73a9d84ae464d8a
Jacob-Hinshaw <jrhinsha@calpoly.edu>
2024-10-23 10:58:15 -0700

    Part 1

--- hw1.py
+++ hw1.py
@@ -3,12 +3,25 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def vowel_count(word:str) -> int:    # A string is inputted, the number of vowels are counted,
+    count = 0                        # and a float value is outputted
+    vowels = "aeiouAEIOU"
+    for char in word:
+        if char in vowels:
+            count += 1
+    return count
 
 # Part 2
+#def short_lists(long:list[list[int]]) -> :
 
+obj1 = 5
+list2 = [obj1]
+list1 = [list2]
+
+print(len(list1))
 
 # Part 3
+
 
 
 # Part 4
--- hw1_test.py
+++ hw1_test.py
@@ -3,13 +3,29 @@ import hw1
 import unittest
 
 
+#def test_first_element_1(self):
+#    input = [[1, 2], [3, 4]]
+#    result = lab4.first_element(input)
+#    expected = [1, 3]
+#    self.assertEqual(expected, result)
+
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
-
+    def test1_vowel_count(self):
+        input = "Hey ThEre"
+        result = hw1.vowel_count(input)
+        expected = 3
+        self.assertEqual(expected, result)
+    def test21_vowel_count(self):
+        input = "Vowels Are Cool"
+        result = hw1.vowel_count(input)
+        expected = 6
+        self.assertEqual(expected, result)
 
     # Part 2
+
 
 
     # Part 3
